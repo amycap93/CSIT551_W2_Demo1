2916fd52d1aef12a6775f6d4007d261d4d6db7ee
Amy Caprioni <Amy Caprioni>
2017-01-31 12:11:20 -0500

    Initial commit

---
+++ app/src/main/java/com/amycap93/csit551_w2_demo1/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.amycap93.csit551_w2_demo1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void goBack(View view){
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
---
+++ app/src/main/java/com/amycap93/csit551_w2_demo1/utils/Utils.java
@@ -0,0 +1,11 @@
+package com.amycap93.csit551_w2_demo1.utils;
+
+/**
+ * Created by Amy on 1/30/2017.
+ */
+
+public class Utils {
+    //final => constant
+    public final static String APP_TAG="ACTIVITY 1 TAG";
+}
+
